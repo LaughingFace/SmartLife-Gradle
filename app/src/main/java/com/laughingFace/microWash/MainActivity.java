b7ff46037b5f0cea2d87ef650743d898f6556eb3
zihao <zihao131125@gmail.com>
2015-07-07 15:05:49 +0800

    ...

--- app/src/main/java/com/laughingFace/microWash/MainActivity.java
+++ app/src/main/java/com/laughingFace/microWash/MainActivity.java
@@ -1,7 +1,7 @@
 package com.laughingFace.microWash;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
