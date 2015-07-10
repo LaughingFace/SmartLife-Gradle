824e343ae296d590a1eeead84d24ce292409ddd5
mathcoder23 <mathcoder@126.com>
2015-07-10 15:22:02 +0800

    --add module connectguide

--- connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/ConnectGuideActivity.java
+++ connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/ConnectGuideActivity.java
@@ -7,12 +7,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import android.widget.TextView;
 import com.laughingFace.microWash.ConnectionGuide.connectGuidePage.CheckPower;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
 /**
  * Created by mathcoder23 on 15-7-9.
  */
 public class ConnectGuideActivity extends FragmentActivity {
     AnimationFragment mCheckPower;
+//    @ViewInject(R.id.id_content)
+//    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/CheckPower.java
+++ connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/CheckPower.java
@@ -6,18 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.TextView;
 import com.laughingFace.microWash.ConnectionGuide.AnimationFragment;
 import com.laughingFace.microWash.ConnectionGuide.R;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * Created by mathcoder23 on 15-7-10.
  */
 public class CheckPower extends AnimationFragment {
-
+//    @ViewInject(R.id.guide_page_check_power_finish)
+//    TextView tvFinish;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.guide_page_check_power, container, false);
+            View view = inflater.inflate(R.layout.guide_page_check_power, container, false);
+            int i = R.id.guide_page_check_power_finish;
+        return view;
     }
 
     @Override
