72d8958f73dc5e2d4d3b8037fd87105a4a6bde20
zihao <zihao131125@gmail.com>
2015-07-13 17:04:19 +0800

    ...

--- connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/RouteConfig.java
+++ connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/RouteConfig.java
@@ -25,8 +25,12 @@ public class RouteConfig extends AnimationFragment{
 
     private View route_config_circle;
     private View route_config_device_icon;
+    private View route_config_wifi_arrow_up;
+    private View route_config_wifi_arrow_down;
+    private View route_config_wifi_arrow;
     private View route_config_phone_arrow_up;
     private View route_config_phone_arrow_down;
+    private View route_config_phone_arrow;
     private View route_config_wifi_icon;
     private View route_config_phone;
     private View route_config_finish;
@@ -46,18 +50,23 @@ public class RouteConfig extends AnimationFragment{
         route_config_device_icon = view.findViewById(R.id.route_config_device_icon);
         route_config_phone_arrow_up = view.findViewById(R.id.route_config_phone_arrow_up);
         route_config_phone_arrow_down = view.findViewById(R.id.route_config_phone_arrow_down);
+        route_config_phone_arrow = view.findViewById(R.id.route_config_phone_arrow);
+        route_config_wifi_arrow_up = view.findViewById(R.id.route_config_wifi_arrow_up);
+        route_config_wifi_arrow_down = view.findViewById(R.id.route_config_wifi_arrow_down);
+        route_config_wifi_arrow = view.findViewById(R.id.route_config_wifi_arrow);
         route_config_wifi_icon = view.findViewById(R.id.route_config_wifi_icon);
         route_config_phone = view.findViewById(R.id.route_config_phone);
         route_config_finish = view.findViewById(R.id.route_config_finish);
 
        route_config_finish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                animationIn();
+           @Override
+           public void onClick(View view) {
+               //route_config_wifi_arrow.setRotation(85);
+               animationIn();
 
 
-            }
-        });
+           }
+       });
 
         return view;
     }
@@ -67,8 +76,16 @@ public class RouteConfig extends AnimationFragment{
         AnimatorSet route_config_in_animation = (AnimatorSet) AnimatorInflater.loadAnimator(mContext, R.animator.route_config_in_animation);
         ArrayList<Animator> childs = route_config_in_animation.getChildAnimations();
 
-       // childs.get(0).setTarget(route_config_circle);
-       // childs.get(0).setDuration(1000);
+        /**
+         * 圆和机器滚入动画
+         */
+        ((AnimatorSet)childs.get(0)).getChildAnimations().get(0).setTarget(route_config_circle);
+        ((AnimatorSet)childs.get(0)).getChildAnimations().get(1).setTarget(route_config_device_icon);
+        childs.get(0).setDuration(2000);
+
+
+
+
 
         route_config_in_animation.start();
     }
