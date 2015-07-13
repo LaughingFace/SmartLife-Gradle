fe7497debf7e8ebc90296461943a3ca35741dd80
zihao <zihao131125@gmail.com>
2015-07-13 23:52:36 +0800

    等待配置完成 入场 退场动画

--- connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/StartConnect.java
+++ connectionguide/src/main/java/com/laughingFace/microWash/ConnectionGuide/connectGuidePage/StartConnect.java
@@ -1,6 +1,11 @@
 package com.laughingFace.microWash.ConnectionGuide.connectGuidePage;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,27 +15,88 @@ import android.view.ViewGroup;
 import com.laughingFace.microWash.ConnectionGuide.AnimationFragment;
 import com.laughingFace.microWash.ConnectionGuide.R;
 
+import java.util.ArrayList;
+
 /**
  * Created by mathcoder23 on 15-7-13.
  */
 public class StartConnect extends AnimationFragment{
     private AnimationFragment.OnChangePage mOnChangePage;
     private Context mContext;
+
+    private View start_connect_circle;
+    private View start_connect_device;
+    private View start_connect_finish;
+    private View start_connect_phone;
+    private View start_connect_hint;
+    private View start_connect_wifi_icon;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.guide_page_start_connect, container, false);
         this.mContext = this.getActivity().getApplicationContext();
+
+        start_connect_circle = view.findViewById(R.id.start_connect_circle);
+        start_connect_device = view.findViewById(R.id.start_connect_device);
+        start_connect_finish = view.findViewById(R.id.start_connect_finish);
+        start_connect_phone = view.findViewById(R.id.start_connect_phone);
+        start_connect_hint = view.findViewById(R.id.start_connect_hint);
+        start_connect_wifi_icon = view.findViewById(R.id.start_connect_wifi_icon);
+
+        start_connect_circle.setVisibility(View.INVISIBLE);
+        start_connect_device.setVisibility(View.INVISIBLE);
+
+        start_connect_wifi_icon.setAlpha(0);
+        start_connect_phone.setAlpha(0);
+        start_connect_finish.setAlpha(0);
+        start_connect_hint.setAlpha(0);
+
+        start_connect_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animationIn();
+                animationOut();
+                start_connect_circle.setVisibility(View.VISIBLE);
+                start_connect_device.setVisibility(View.VISIBLE);
+            }
+        });
+
         return view;
     }
 
     @Override
     public void animationIn() {
+        AnimatorSet start_connect_in_animation = (AnimatorSet) AnimatorInflater.loadAnimator(mContext, R.animator.start_connect_in_animation);
+        ArrayList<Animator> childs = start_connect_in_animation.getChildAnimations();
+
+        /**
+         * 圆和机器滚入动画
+         */
+        ((AnimatorSet)childs.get(0)).getChildAnimations().get(0).setTarget(start_connect_circle);
+        ((AnimatorSet)childs.get(0)).getChildAnimations().get(1).setTarget(start_connect_device);
+        childs.get(0).setDuration(2000);
+
+        /**
+         * 手机和提示文字淡 入动画
+         */
+        ((AnimatorSet)childs.get(1)).getChildAnimations().get(0).setTarget(start_connect_phone);
+        ((AnimatorSet)childs.get(1)).getChildAnimations().get(1).setTarget(start_connect_hint);
+        childs.get(1).setDuration(2500);
+
+
+
+        start_connect_in_animation.start();
 
     }
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public void animationOut() {
+
+        start_connect_circle.animate().setStartDelay(4000).translationY(100).scaleX(10).scaleY(10).setDuration(1000).start();
+        start_connect_phone.animate().setStartDelay(4000).alpha(0).scaleX(0).scaleY(0).setDuration(1000).start();
+        start_connect_device.animate().setStartDelay(4000).alpha(0).scaleX(0).scaleY(0).setDuration(1000).start();
+        start_connect_hint.animate().setStartDelay(4000).alpha(0).scaleX(0).scaleY(0).setDuration(1000).start();
 
     }
 
